f2365d4b0519d9e63340635e3316686c5743f0d9
MadhurAroraaa <aroramadhur0248@gmail.com>
2025-09-13 02:14:06 +0530

    commit

--- components/Home/Info/Info.tsx
+++ components/Home/Info/Info.tsx
@@ -13,18 +13,13 @@ const epilogue = Epilogue({
 const Info = () => {
     return (
         <div className='pt-16 pb-16'>
-            <div className='w-[80%] mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10 items-center'>
-                {/* IMAGE CONTENT */}
-                <div>
-                    <Image src="/images/maddy.png" alt='Hello Ji' width={900}
-                        height={900}
-                        className="w-[900px] h-[900px] object-contain" />
-                </div>
+            <div className='w-[50%] mx-auto flex gap-10 items-center justify-center text-center'>
+
                 {/* TEXT CONTENT */}
                 <div>
                     <h1 className={`text-3xl md:text-4xl lg:text-5xl font-extrabold leading-8 lg:leading-16 uppercase bg-gradient-to-r from-sky-500 via-25% to-blue-800 bg-clip-text text-transparent ${epilogue.className}`}>Get applications from the world best talents.</h1>
                     <p className='mt-6 text-gray-300'>Search all the open positions on the web. Get your own personalized salary estimate. Read reviews on over 600,000 companies worldwide.</p>
-                    <div className='mt-8'>
+                    <div className='mt-8 flex items-center flex-col'>
                         <div className='flex items-center space-x-2 mb-4'>
                             <BiCheck className='w-7 h-7 text-sky-500' />
                             <span className='text-gray-300 font-medium'>Bring to the table win-win survival</span>
